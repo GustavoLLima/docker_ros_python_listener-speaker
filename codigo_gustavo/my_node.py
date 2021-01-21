b33c52d823a9f64fcb77b13900627c89de824306
Gustavo <gustavolameirao@gmail.com>
2021-01-21 18:55:46 +0000

    Primeiro push de teste

---
+++ codigo_gustavo/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from codigo_gustavo.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+package_name = 'codigo_gustavo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='root',
+    maintainer_email='root@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'my_node = codigo_gustavo.my_node:main',
+            'talker = codigo_gustavo.publisher_member_function:main',
+            'listener = codigo_gustavo.subscriber_member_function:main',
+        ],
+    },
+)
