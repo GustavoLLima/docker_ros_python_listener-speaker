11286217db547086da709f3ceff8038771d31273
GustavoLLima <gustavolameirao@gmail.com>
2021-02-01 18:18:18 -0300

    Update subscriber_member_function.py

--- codigo_gustavo/subscriber_member_function.py
+++ codigo_gustavo/subscriber_member_function.py
@@ -1,3 +1,7 @@
+import socket
+import sys
+import json
+
 import rclpy
 from rclpy.node import Node
 
@@ -16,10 +20,10 @@ class MinimalSubscriber(Node):
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
-        if(msg.data == "1"):
-            self.get_logger().info('Andar, pois ouvi: "%s"' % msg.data)
-        else:
-            self.get_logger().info('Parar, pois ouvi: "%s"' % msg.data)
+        # if(msg.data == "1"):
+        #     self.get_logger().info('Andar, pois ouvi: "%s"' % msg.data)
+        # else:
+        #     self.get_logger().info('Parar, pois ouvi: "%s"' % msg.data)
         #self.get_logger().info('I heard: "%s"' % msg.data)
 
 
@@ -39,6 +43,53 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+# import rclpy
+# from rclpy.node import Node
+
+# from std_msgs.msg import String
+
+
+# class MinimalSubscriber(Node):
+
+#     def __init__(self):
+#         super().__init__('minimal_subscriber')
+#         self.subscription = self.create_subscription(
+#             String,
+#             'topic',
+#             self.listener_callback,
+#             10)
+#         self.subscription  # prevent unused variable warning
+
+#     def listener_callback(self, msg):
+#         if(msg.data == "1"):
+#             self.get_logger().info('Andar, pois ouvi: "%s"' % msg.data)
+#         else:
+#             self.get_logger().info('Parar, pois ouvi: "%s"' % msg.data)
+#         #self.get_logger().info('I heard: "%s"' % msg.data)
+
+
+# def main(args=None):
+#     rclpy.init(args=args)
+
+#     minimal_subscriber = MinimalSubscriber()
+
+#     rclpy.spin(minimal_subscriber)
+
+#     # Destroy the node explicitly
+#     # (optional - otherwise it will be done automatically
+#     # when the garbage collector destroys the node object)
+#     minimal_subscriber.destroy_node()
+#     rclpy.shutdown()
+
+
+# if __name__ == '__main__':
+#     main()
 
 
 
