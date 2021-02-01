1251e682a678edfdf9f1defe00778c34487679ba
GustavoLLima <gustavolameirao@gmail.com>
2021-02-01 19:33:09 -0300

    Update publisher_member_function.py

--- codigo_gustavo/publisher_member_function.py
+++ codigo_gustavo/publisher_member_function.py
@@ -16,22 +16,28 @@ from std_msgs.msg import String
 msg_to_send = ""
 
 
+import rclpy
+import random
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
 class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
         self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 10  # seconds
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = String()
-        # if (random.randint(1,10) >= 5):
-        #   msg.data = '1'
-        # else:
-        #   msg.data = '0'
-        msg.data = msg_to_send
+        if (random.randint(1,10) >= 5):
+          msg.data = '1'
+        else:
+          msg.data = '0'
         
         #msg.data = 'Hello World: %d' % self.i
         self.publisher_.publish(msg)
@@ -40,48 +46,17 @@ class MinimalPublisher(Node):
 
 
 def main(args=None):
+    rclpy.init(args=args)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("talker", 9999))
-    s.listen(2)
+    minimal_publisher = MinimalPublisher()
 
-    while True:
-        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # s.bind(("python_server", 9999))
-        # s.listen(2)
-        conn, addr = s.accept()
-        print("Conexão estabelecida com %s" % str(addr))
-        received_message = bytes.decode(conn.recv(1024))
-        print ("Mensagem recebida:")
-        print(received_message)
-        global msg_to_send
-        msg_to_send = "received_message"
+    rclpy.spin(minimal_publisher)
 
-        #x = json.loads(received_message, object_hook=lambda d: SimpleNamespace(**d))
-        #print(x.id, x.action)
-
-        # m = {"id": int(received_message), "action": "stop"} # a real dict.
-        # command = json.dumps(m)
-
-        #command = "dsadsasdasadsda"
-        #print ("Enviando para o cliente:"+command)
-
-        #conn.sendall(command.encode('ascii'))
-
-
-
-        #Código ROS
-        rclpy.init(args=args)
-
-        minimal_publisher = MinimalPublisher()
-
-        rclpy.spin(minimal_publisher)
-
-        # Destroy the node explicitly
-        # (optional - otherwise it will be done automatically
-        # when the garbage collector destroys the node object)
-        minimal_publisher.destroy_node()
-        rclpy.shutdown()
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == '__main__':
