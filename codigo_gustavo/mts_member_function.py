0ad05d08b2a887e94a9debd67ff01ab18d5add01
GustavoLLima <gustavolameirao@gmail.com>
2021-02-28 16:44:48 -0300

    Update mts_member_function.py

--- codigo_gustavo/mts_member_function.py
+++ codigo_gustavo/mts_member_function.py
@@ -9,7 +9,6 @@ from std_msgs.msg import String
 
 from types import SimpleNamespace
 
-msg_to_send = ""
 
 class MinimalSubscriber(Node):
 
@@ -23,45 +22,28 @@ class MinimalSubscriber(Node):
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, msg):
+        # if(msg.data == "1"):
+        #     self.get_logger().info('Andar, pois ouvi: "%s"' % msg.data)
+        # else:
+        #     self.get_logger().info('Parar, pois ouvi: "%s"' % msg.data)
         self.get_logger().info('I heard: "%s"' % msg.data)
-        # x = json.loads(msg.data, object_hook=lambda d: SimpleNamespace(**d))
+        x = json.loads(msg.data, object_hook=lambda d: SimpleNamespace(**d))
         print(x.id, x.action)
 
-        global msg_to_send
-        msg_to_send = msg.data
+        m = {"id": x.id, "action": action}
+        command = json.dumps(m)
+        print ("Enviando para o modelo:"+command)
 
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # print(socket.gethostname())
+        sock.connect(('modelo', 9999))
+        sock.sendall(command.encode())
 
-class MinimalPublisher(Node):
-
-    def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic2', 10)
-        while True:
-            self.timer_callback()
-
-    def timer_callback(self):
-
-        test_string = my_hostname
-        my_id = int(''.join(filter(lambda i: i.isdigit(), test_string)))
-        # print (str(my_id))
-
-        #position = random.randint(0,100)
-
-        #m = {"id": my_id, "position": position} # a real dict.
-        command = json.loads(msg_to_send, object_hook=lambda d: SimpleNamespace(**d))
-        #command = json.dumps(m)
-
-        msg = String()
-        msg.data = msg_to_send
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
 
     minimal_subscriber = MinimalSubscriber()
-
-    minimal_publisher = MinimalPublisher()
 
     rclpy.spin(minimal_subscriber)
 
@@ -69,8 +51,6 @@ def main(args=None):
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
     minimal_subscriber.destroy_node()
-
-    minimal_publisher.destroy_node()
     rclpy.shutdown()
 
 
