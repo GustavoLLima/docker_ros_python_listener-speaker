958b33008ccddc6e27b45e252c9497726712f876
GustavoLLima <gustavolameirao@gmail.com>
2021-03-01 18:57:21 -0300

    Update n_publisher_member_function.py

--- codigo_gustavo/n_publisher_member_function.py
+++ codigo_gustavo/n_publisher_member_function.py
@@ -6,8 +6,6 @@ from std_msgs.msg import String
 
 import json
 
-
-
 def main(args=None):
   rclpy.init(args=args)
 
@@ -24,7 +22,7 @@ def main(args=None):
     if (i > 3):
         m = {"id": 1, "position": 10}
         msg.data = json.dumps(m)
-        publisher.publish(msg.data)
+        publisher.publish(msg)
         i += 1
     sleep(0.5)  # seconds
 
@@ -37,3 +35,43 @@ def main(args=None):
 
 if __name__ == '__main__':
   main()
+
+
+
+
+# from time import sleep
+
+# import rclpy
+
+# from std_msgs.msg import String
+
+
+
+# def main(args=None):
+#   rclpy.init(args=args)
+
+#   node = rclpy.create_node('minimal_publisher')
+
+#   publisher = node.create_publisher(String, 'topic', 10)
+
+#   msg = String()
+#   senden = true
+#   i = 0
+#   while rclpy.ok():
+#     msg.data = 'Hello World: %d' % i
+#     i += 1
+#     #node.get_logger().info('Publishing: "%s"' % msg.data)
+#     If send:
+#         publisher.publish(msg)
+#         Send= false
+#     sleep(0.5)  # seconds
+
+# # Destroy the node explicitly
+# # (optional - otherwise it will be done automatically
+# # when the garbage collector destroys the node object)
+# node.destroy_node()
+# rclpy.shutdown()
+
+
+# if __name__ == '__main__':
+#   main()
