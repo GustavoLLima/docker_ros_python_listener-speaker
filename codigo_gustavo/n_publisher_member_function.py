30a7b128a372c5fe490f72f18274abb1097b7de6
GustavoLLima <gustavolameirao@gmail.com>
2021-03-01 18:42:17 -0300

    Update n_publisher_member_function.py

--- codigo_gustavo/n_publisher_member_function.py
+++ codigo_gustavo/n_publisher_member_function.py
@@ -27,7 +27,7 @@ def main(args=None):
 # Destroy the node explicitly
 # (optional - otherwise it will be done automatically
 # when the garbage collector destroys the node object)
-node.destroy_node()
+#node.destroy_node()
 rclpy.shutdown()
 
 
