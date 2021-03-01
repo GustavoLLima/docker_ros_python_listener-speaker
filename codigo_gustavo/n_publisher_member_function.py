aae9b0aa31e789596e0a9f24d3162590d21fbb32
GustavoLLima <gustavolameirao@gmail.com>
2021-03-01 18:35:05 -0300

    Add files via upload

---
+++ codigo_gustavo/n_publisher_member_function.py
@@ -0,0 +1,35 @@
+from time import sleep
+
+import rclpy
+
+from std_msgs.msg import String
+
+
+
+def main(args=None):
+  rclpy.init(args=args)
+
+  node = rclpy.create_node('minimal_publisher')
+
+  publisher = node.create_publisher(String, 'topic', 10)
+
+  msg = String()
+  senden = true
+  i = 3
+  while rclpy.ok():
+    msg.data = 'Hello World: %d' % i
+    #node.get_logger().info('Publishing: "%s"' % msg.data)
+    if (i > 3):
+        publisher.publish(msg)
+        i += 1
+    sleep(0.5)  # seconds
+
+# Destroy the node explicitly
+# (optional - otherwise it will be done automatically
+# when the garbage collector destroys the node object)
+node.destroy_node()
+rclpy.shutdown()
+
+
+if __name__ == '__main__':
+  main()
