227b67872c4265cbe588df0570a74654c532effc
GustavoLLima <gustavolameirao@gmail.com>
2021-03-03 17:16:24 -0300

    Update multiple_publisher_member_function.py

--- codigo_gustavo/multiple_publisher_member_function.py
+++ codigo_gustavo/multiple_publisher_member_function.py
@@ -1,352 +1,110 @@
 import socket
 import sys
 import json
-from random import randrange
-from time import time, sleep
-
-from types import SimpleNamespace
-
-import os
-my_hostname = os.environ['my_hostname']
 
 import rclpy
-import random
 from rclpy.node import Node
 
 from std_msgs.msg import String
 
-msg_to_send = ""
+from types import SimpleNamespace
+
+import os
+
+queue_size = int(os.environ['queue_size'])
+queue = []
 
 
-class MinimalPublisher(Node):
+class MinimalSubscriber(Node):
 
     def __init__(self):
-        super().__init__('minimal_publisher')
-        # self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 10  # seconds
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
 
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((my_hostname, 9999))
-        s.listen(2)
-
-        while True:
-            # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # s.bind(("python_server", 9999))
-            # s.listen(2)
-            conn, addr = s.accept()
-            print("Conexão estabelecida com %s" % str(addr))
-            received_message = bytes.decode(conn.recv(1024))
-            print ("Mensagem recebida:")
-            print(received_message)
-            global msg_to_send
-            msg_to_send = received_message
-
-            x = json.loads(received_message, object_hook=lambda d: SimpleNamespace(**d))
-            #print(x.id, x.position)
-
-            # test_string = my_hostname
-            # my_id = int(''.join(filter(lambda i: i.isdigit(), test_string)))
-            topic = "final_output_agent"+x.id
-            self.publisher_ = self.create_publisher(String, topic, 10)
-
-
-            self.timer_callback()
-
-
-        # self.timer = self.create_timer(timer_period, self.timer_callback)
-        # self.i = 0
-
-    def timer_callback(self):
-        msg = String()
-        # if (random.randint(1,10) >= 5):
-        #   msg.data = '1'
+    def listener_callback(self, msg):
+        # if(msg.data == "1"):
+        #     self.get_logger().info('Andar, pois ouvi: "%s"' % msg.data)
         # else:
-        #   msg.data = '0'
-        msg.data = msg_to_send
-        
-        #msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        # self.i += 1
+        #     self.get_logger().info('Parar, pois ouvi: "%s"' % msg.data)
+        self.get_logger().info('I heard: "%s"' % msg.data)
+        # x = json.loads(msg.data, object_hook=lambda d: SimpleNamespace(**d))
+        # #print(x.id, x.position)
+
+        # m = {"id": x.id, "position": x.position}
+        # command = json.dumps(m)
+        global queue
+        queue.append(msg.data)
+        print ("Comando adicionado na fila:"+msg.data)
+        #print ("Enviando para o modelo:"+command)
+
+        print("len queue: "+str(len(queue))+" queue_size: "+str(queue_size))
+        print(len(queue) == queue_size)
+        if (len(queue) == queue_size):
+            print ("Fila atingiu o tamanho para envio")
+
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #m = {"id": x.id, "position": x.position}
+            command = json.dumps(queue)
+            print("Enviando para o modelo:"+command)
+            
+            # print(socket.gethostname())
+            sock.connect(('modelo', 9999))
+            sock.sendall(command.encode())
+            # while len(queue) > 0:
+            #     elemento = queue.pop(0)
+            #     print(elemento)
+            #     x = json.loads(elemento, object_hook=lambda d: SimpleNamespace(**d))
+            #     #print(x.id, x.position)
+
+            #     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            #     m = {"id": x.id, "position": x.position}
+            #     command = json.dumps(m)
+            #     print("Enviando para o modelo:"+command)
+                
+            #     # print(socket.gethostname())
+            #     sock.connect(('modelo', 9999))
+            #     sock.sendall(command.encode())
+            queue = []
+
+        # print("len queue: "+str(len(queue))+" queue_size: "+str(queue_size))
+        # print(len(queue) == queue_size)
+        # if (len(queue) == queue_size):
+        #     print ("Fila atingiu o tamanho para envio")
+        #     while len(queue) > 0:
+        #         elemento = queue.pop(0)
+        #         print(elemento)
+        #         x = json.loads(elemento, object_hook=lambda d: SimpleNamespace(**d))
+        #         #print(x.id, x.position)
+
+        #         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #         m = {"id": x.id, "position": x.position}
+        #         command = json.dumps(m)
+        #         print("Enviando para o modelo:"+command)
+                
+        #         # print(socket.gethostname())
+        #         sock.connect(('modelo', 9999))
+        #         sock.sendall(command.encode())
+        #     queue = []
 
 
 def main(args=None):
-
-    # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # s.bind(("talker", 9999))
-    # s.listen(2)
-
-    # while True:
-    #     # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     # s.bind(("python_server", 9999))
-    #     # s.listen(2)
-    #     conn, addr = s.accept()
-    #     print("Conexão estabelecida com %s" % str(addr))
-    #     received_message = bytes.decode(conn.recv(1024))
-    #     print ("Mensagem recebida:")
-    #     print(received_message)
-    #     global msg_to_send
-    #     msg_to_send = received_message
-
-    #x = json.loads(received_message, object_hook=lambda d: SimpleNamespace(**d))
-    #print(x.id, x.action)
-
-    # m = {"id": int(received_message), "action": "stop"} # a real dict.
-    # command = json.dumps(m)
-
-    #command = "dsadsasdasadsda"
-    #print ("Enviando para o cliente:"+command)
-
-    #conn.sendall(command.encode('ascii'))
-
-
-
-    #Código ROS
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    minimal_subscriber = MinimalSubscriber()
 
-    rclpy.spin(minimal_publisher)
+    rclpy.spin(minimal_subscriber)
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
+    minimal_subscriber.destroy_node()
     rclpy.shutdown()
 
 
 if __name__ == '__main__':
     main()
-
-# FUNCIONANDO, MAS QUERENDO DEIXAR O TALKER COMO UM ÚNICO CONTAINER QUE MANDA PRA TODOS, DINAMICAMENTE COM BASE NO ID DA MENSAGEM
-
-# import socket
-# import sys
-# import json
-# from random import randrange
-# from time import time, sleep
-
-# from types import SimpleNamespace
-
-# import os
-# my_hostname = os.environ['my_hostname']
-
-# import rclpy
-# import random
-# from rclpy.node import Node
-
-# from std_msgs.msg import String
-
-# msg_to_send = ""
-
-
-# class MinimalPublisher(Node):
-
-#     def __init__(self):
-#         super().__init__('minimal_publisher')
-#         # self.publisher_ = self.create_publisher(String, 'topic', 10)
-#         timer_period = 10  # seconds
-
-
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         s.bind((my_hostname, 9999))
-#         s.listen(2)
-
-#         while True:
-#             # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#             # s.bind(("python_server", 9999))
-#             # s.listen(2)
-#             conn, addr = s.accept()
-#             print("Conexão estabelecida com %s" % str(addr))
-#             received_message = bytes.decode(conn.recv(1024))
-#             print ("Mensagem recebida:")
-#             print(received_message)
-#             global msg_to_send
-#             msg_to_send = received_message
-
-#             test_string = my_hostname
-#             my_id = int(''.join(filter(lambda i: i.isdigit(), test_string)))
-#             topic = "final_output_agent"+str(my_id)
-#             self.publisher_ = self.create_publisher(String, topic, 10)
-
-
-#             self.timer_callback()
-
-
-#         # self.timer = self.create_timer(timer_period, self.timer_callback)
-#         # self.i = 0
-
-#     def timer_callback(self):
-#         msg = String()
-#         # if (random.randint(1,10) >= 5):
-#         #   msg.data = '1'
-#         # else:
-#         #   msg.data = '0'
-#         msg.data = msg_to_send
-        
-#         #msg.data = 'Hello World: %d' % self.i
-#         self.publisher_.publish(msg)
-#         self.get_logger().info('Publishing: "%s"' % msg.data)
-#         # self.i += 1
-
-
-# def main(args=None):
-
-#     # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     # s.bind(("talker", 9999))
-#     # s.listen(2)
-
-#     # while True:
-#     #     # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     #     # s.bind(("python_server", 9999))
-#     #     # s.listen(2)
-#     #     conn, addr = s.accept()
-#     #     print("Conexão estabelecida com %s" % str(addr))
-#     #     received_message = bytes.decode(conn.recv(1024))
-#     #     print ("Mensagem recebida:")
-#     #     print(received_message)
-#     #     global msg_to_send
-#     #     msg_to_send = received_message
-
-#     #x = json.loads(received_message, object_hook=lambda d: SimpleNamespace(**d))
-#     #print(x.id, x.action)
-
-#     # m = {"id": int(received_message), "action": "stop"} # a real dict.
-#     # command = json.dumps(m)
-
-#     #command = "dsadsasdasadsda"
-#     #print ("Enviando para o cliente:"+command)
-
-#     #conn.sendall(command.encode('ascii'))
-
-
-
-#     #Código ROS
-#     rclpy.init(args=args)
-
-#     minimal_publisher = MinimalPublisher()
-
-#     rclpy.spin(minimal_publisher)
-
-#     # Destroy the node explicitly
-#     # (optional - otherwise it will be done automatically
-#     # when the garbage collector destroys the node object)
-#     minimal_publisher.destroy_node()
-#     rclpy.shutdown()
-
-
-# if __name__ == '__main__':
-#     main()
-
-### FUNCIONANDO, SÓ SEPARANDO PRA MANDAR PRA TÓPICOS SEPARADOS E NÃO ENTRAR EM CONFLITO COM O PRINCIPAL
-# import socket
-# import sys
-# import json
-# from random import randrange
-# from time import time, sleep
-
-# from types import SimpleNamespace
-
-# import os
-# my_hostname = os.environ['my_hostname']
-
-# import rclpy
-# import random
-# from rclpy.node import Node
-
-# from std_msgs.msg import String
-
-# msg_to_send = ""
-
-
-# class MinimalPublisher(Node):
-
-#     def __init__(self):
-#         super().__init__('minimal_publisher')
-#         self.publisher_ = self.create_publisher(String, 'topic', 10)
-#         timer_period = 10  # seconds
-
-
-#         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         s.bind((my_hostname, 9999))
-#         s.listen(2)
-
-#         while True:
-#             # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#             # s.bind(("python_server", 9999))
-#             # s.listen(2)
-#             conn, addr = s.accept()
-#             print("Conexão estabelecida com %s" % str(addr))
-#             received_message = bytes.decode(conn.recv(1024))
-#             print ("Mensagem recebida:")
-#             print(received_message)
-#             global msg_to_send
-#             msg_to_send = received_message
-#             self.timer_callback()
-
-
-#         # self.timer = self.create_timer(timer_period, self.timer_callback)
-#         # self.i = 0
-
-#     def timer_callback(self):
-#         msg = String()
-#         # if (random.randint(1,10) >= 5):
-#         #   msg.data = '1'
-#         # else:
-#         #   msg.data = '0'
-#         msg.data = msg_to_send
-        
-#         #msg.data = 'Hello World: %d' % self.i
-#         self.publisher_.publish(msg)
-#         self.get_logger().info('Publishing: "%s"' % msg.data)
-#         # self.i += 1
-
-
-# def main(args=None):
-
-#     # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     # s.bind(("talker", 9999))
-#     # s.listen(2)
-
-#     # while True:
-#     #     # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     #     # s.bind(("python_server", 9999))
-#     #     # s.listen(2)
-#     #     conn, addr = s.accept()
-#     #     print("Conexão estabelecida com %s" % str(addr))
-#     #     received_message = bytes.decode(conn.recv(1024))
-#     #     print ("Mensagem recebida:")
-#     #     print(received_message)
-#     #     global msg_to_send
-#     #     msg_to_send = received_message
-
-#     #x = json.loads(received_message, object_hook=lambda d: SimpleNamespace(**d))
-#     #print(x.id, x.action)
-
-#     # m = {"id": int(received_message), "action": "stop"} # a real dict.
-#     # command = json.dumps(m)
-
-#     #command = "dsadsasdasadsda"
-#     #print ("Enviando para o cliente:"+command)
-
-#     #conn.sendall(command.encode('ascii'))
-
-
-
-#     #Código ROS
-#     rclpy.init(args=args)
-
-#     minimal_publisher = MinimalPublisher()
-
-#     rclpy.spin(minimal_publisher)
-
-#     # Destroy the node explicitly
-#     # (optional - otherwise it will be done automatically
-#     # when the garbage collector destroys the node object)
-#     minimal_publisher.destroy_node()
-#     rclpy.shutdown()
-
-
-# if __name__ == '__main__':
-#     main()
