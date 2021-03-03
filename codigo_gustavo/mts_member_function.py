c8aae7e1e0b60e9f58321ce566019805006bd144
GustavoLLima <gustavolameirao@gmail.com>
2021-03-03 15:03:35 -0300

    Update mts_member_function.py

--- codigo_gustavo/mts_member_function.py
+++ codigo_gustavo/mts_member_function.py
@@ -9,6 +9,8 @@ from std_msgs.msg import String
 
 from types import SimpleNamespace
 
+queue_size = os.environ['queue_size']
+
 
 class MinimalSubscriber(Node):
 
@@ -20,6 +22,7 @@ class MinimalSubscriber(Node):
             self.listener_callback,
             10)
         self.subscription  # prevent unused variable warning
+        self.queue = []
 
     def listener_callback(self, msg):
         # if(msg.data == "1"):
@@ -27,17 +30,31 @@ class MinimalSubscriber(Node):
         # else:
         #     self.get_logger().info('Parar, pois ouvi: "%s"' % msg.data)
         self.get_logger().info('I heard: "%s"' % msg.data)
-        x = json.loads(msg.data, object_hook=lambda d: SimpleNamespace(**d))
-        print(x.id, x.position)
+        # x = json.loads(msg.data, object_hook=lambda d: SimpleNamespace(**d))
+        # #print(x.id, x.position)
 
-        m = {"id": x.id, "position": x.position}
-        command = json.dumps(m)
-        print ("Enviando para o modelo:"+command)
+        # m = {"id": x.id, "position": x.position}
+        # command = json.dumps(m)
+        self.queue.append(msg.data)
+        print ("Comando adicionado na fila:"+msg.data)
+        #print ("Enviando para o modelo:"+command)
 
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # print(socket.gethostname())
-        sock.connect(('modelo', 9999))
-        sock.sendall(command.encode())
+        if (length(self.queue) == queue_size)
+            print ("Fila atingiu o tamanho para envio")
+            while len(teste) > 0:
+                elemento = teste.pop(0)
+                print(elemento)
+                x = json.loads(msg.elemento, object_hook=lambda d: SimpleNamespace(**d))
+                #print(x.id, x.position)
+
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                m = {"id": x.id, "position": x.position}
+                command = json.dumps(m)
+                print("Enviando para o modelo:"+command)
+                
+                # print(socket.gethostname())
+                sock.connect(('modelo', 9999))
+                sock.sendall(command.encode())
 
 
 def main(args=None):
