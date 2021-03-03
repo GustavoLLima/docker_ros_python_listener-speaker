35127e18dee09dcf06d8cbe9b037a3ff9d7e9f54
GustavoLLima <gustavolameirao@gmail.com>
2021-03-03 16:05:33 -0300

    Update mts_member_function.py

--- codigo_gustavo/mts_member_function.py
+++ codigo_gustavo/mts_member_function.py
@@ -43,6 +43,7 @@ class MinimalSubscriber(Node):
         #print ("Enviando para o modelo:"+command)
 
         print("len queue: "+str(len(queue))+" queue_size: "+str(queue_size))
+        print(len(queue) == queue_size)
         if (len(queue) == queue_size):
             print ("Fila atingiu o tamanho para envio")
             while len(teste) > 0:
